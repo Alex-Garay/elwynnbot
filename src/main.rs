a9416771d0bbb9b455f0cc3c89b74b16cfcd3139
Alex Garay <alex.garay94@gmail.com>
2023-04-14 23:19:52 -0700

    updated get_pid to work with latest windows crate, rewrote simpler byte to string, wrote documentation

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,23 @@
 mod memory;
 use memory::get_pid;
 use process_memory::*;
-
 fn main() -> std::io::Result<()> {
-
     let process_handle = get_pid("Wow.exe").try_into_process_handle()?;
 
     let mut player_name = DataMember::<[u8; 8]>::new(process_handle);
 
-    // Sets the memory address for a player's name.
+    // // Sets the memory address for a player's name.
     player_name.set_offset(vec![0x00C79D18]);
 
-    // Read name of player in bytes.
+    // // Read name of player in bytes.
     let string_bytes = unsafe {
         player_name.read().unwrap()
     };
 
-    println!("String Number: {:?}", string_bytes);
-
-    // Turns Bytes into a String
+    // // Turns Bytes into a String
     let binding = String::from_utf8_lossy(&string_bytes);
 
-    // Removes null 0 bytes from the string.
+    // // Removes null 0 bytes from the string.
     let result_string = binding.split('\0').next().unwrap();
 
     println!("Name: {:?}", &result_string);
--- src/memory/get_pid.rs
+++ src/memory/get_pid.rs
@@ -1,6 +1,6 @@
+use std::mem::MaybeUninit;
 mod windows {
     pub(crate) use windows::Win32::{
-        Foundation::{CHAR, MAX_PATH},
         System::Diagnostics::ToolHelp::{
             CreateToolhelp32Snapshot, Process32First, Process32Next, PROCESSENTRY32,
             TH32CS_SNAPPROCESS,
@@ -9,45 +9,43 @@ mod windows {
 }
 
 pub fn get_pid(process_name: &str) -> process_memory::Pid {
-    /// A helper function to turn a CHAR array to a String
-    fn utf8_to_string(bytes: &[windows::CHAR]) -> String {
-        use std::ffi::CStr;
-        unsafe {
-            CStr::from_ptr(bytes.as_ptr() as *const i8)
-                .to_string_lossy()
-                .into_owned()
+    // PROCESSENTRY32: Describes an entry from a list of the processes residing in the system address space when a snapshot was taken.
+    // MaybeUninit::zeroed().assume_init() makes 0 as the default value for all properties.
+    // dwSize: The size of the structure, in bytes. Before calling the Process32First function, set this member to sizeof(PROCESSENTRY32). If you do not initialize dwSize, Process32First fails.
+    let mut entry: windows::PROCESSENTRY32 = unsafe {
+        let mut entry: windows::PROCESSENTRY32 = MaybeUninit::zeroed().assume_init();
+        entry.dwSize = std::mem::size_of::<windows::PROCESSENTRY32>() as u32;
+        entry
+    };
+
+    // CreateToolhelp32Snapshot: Takes a snapshot of the specified processes, as well as the heaps, modules, and threads used by these processes.
+    // dwFlags: The portions of the system to be included in the snapshot. This parameter can be one or more of the following values.
+    // TH32CS_SNAPPROCESS: Includes all processes in the system in the snapshot.
+    let snapshot = unsafe {
+        match windows::CreateToolhelp32Snapshot(windows::TH32CS_SNAPPROCESS, 0) {
+        Ok(shot) =>  shot,
+        _ => return 0,
         }
+    };
+
+    // Process32First: Retrieves information about the first process encountered in a system snapshot.
+    if unsafe {windows::Process32First(snapshot, &mut entry) == false} {
+        return 0
     }
 
-    let mut entry = windows::PROCESSENTRY32 {
-        dwSize: std::mem::size_of::<windows::PROCESSENTRY32>() as u32,
-        cntUsage: 0,
-        th32ProcessID: 0,
-        th32DefaultHeapID: 0,
-        th32ModuleID: 0,
-        cntThreads: 0,
-        th32ParentProcessID: 0,
-        pcPriClassBase: 0,
-        dwFlags: 0,
-        szExeFile: [windows::CHAR(0); windows::MAX_PATH as usize],
-    };
-    unsafe {
-        // On Error return 0 as the pid. Maybe this function should instead return itself a Result
-        // to indicate if a pid has been found?
-        let snapshot = if let Ok(snapshot) =
-            windows::CreateToolhelp32Snapshot(windows::TH32CS_SNAPPROCESS, 0)
-        {
-            snapshot
-        } else {
+    loop {
+        // Converts the current process's name from bytes to string.
+        let binding = String::from_utf8_lossy(&entry.szExeFile);
+        // Removes all the extra null zeros in the string. Example: "Wow.exe/0/0/0/0/0/0/0/0"
+        let santized_binding = binding.split("\0").next().unwrap();
+        // Compare our current process's name with the wanted process's name.
+        if santized_binding == process_name {
+            // Returns the process id (PID)
+            return entry.th32ProcessID;
+        }
+
+        if unsafe { windows::Process32Next(snapshot, &mut entry) == false} {
             return 0;
-        };
-        if windows::Process32First(snapshot, &mut entry) == true {
-            while windows::Process32Next(snapshot, &mut entry) == true {
-                if utf8_to_string(&entry.szExeFile) == process_name {
-                    return entry.th32ProcessID;
-                }
-            }
         }
     }
-    0
 }
