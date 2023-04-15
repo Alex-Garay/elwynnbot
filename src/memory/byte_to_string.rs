d4ab29224a718086ec432fe232a89b863963626a
Alex Garay <alex.garay94@gmail.com>
2023-04-15 14:17:49 -0700

    object manager in progress

--- src/memory/byte_to_string.rs
+++ src/memory/byte_to_string.rs
@@ -1,7 +1,7 @@
 pub fn byte_to_string(bytes: &[u8]) -> String {
     // Turns Bytes into a String
     let binding = String::from_utf8_lossy(bytes);
-    // Removes null 0 bytes from the string.
+    // Removes all the extra null zeros in the string. Example: "Wow.exe/0/0/0/0/0/0/0/0"
     let santized_binding = binding.split("\0").next().unwrap();
     santized_binding.to_owned()
 }
